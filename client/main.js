5039613525591c04ef63ecf88976aff175048601
Dimitris Tsoukalis <tsoukalis_d@hotmail.com>
2020-10-07 20:13:08 +0300

    Tutorial completed without accounts

---
+++ client/main.js
@@ -0,0 +1,16 @@
+/* import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
+
+angular.module('simple-todos',[
+  angularMeteor,
+  todosList.name
+]); */
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
+ 
+angular.module('simple-todos', [
+  angularMeteor,
+  todosList.name
+]);
